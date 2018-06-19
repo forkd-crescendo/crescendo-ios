76636eed6ff774a489507f0171a2e5328a90200d
Roosevelt <panta_85.x@hotmail.com>
2018-06-19 17:15:19 -0500

    collectionView

---
+++ crescendo-ios/Model/Artist.swift
@@ -0,0 +1,20 @@
+//
+//  Artist.swift
+//  crescendo-ios
+//
+//  Created by Roosevelt Pantaleon on 03/06/18.
+//  Copyright © 2018 forkd. All rights reserved.
+//
+
+import Foundation
+
+class Artist {
+    var name: String
+    var genre: String
+    
+    init(name: String, genre: String) {
+        self.name = name
+        self.genre = genre
+    }
+    
+}
---
+++ crescendo-ios/ViewController/ArtistViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ArtistViewController.swift
+//  crescendo-ios
+//
+//  Created by Roosevelt Pantaleon on 03/06/18.
+//  Copyright © 2018 forkd. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class ArtistCell: UICollectionViewCell {
+    @IBOutlet var name: UILabel!
+    @IBOutlet var genre: UILabel!
+    
+    func updateViews(from artist: Artist) {
+        name.text = artist.name
+        genre.text = artist.genre
+    }
+}
+
+class ArtistViewController: UICollectionViewController {
+    var artists: [Artist] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        //  self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+        generateMockData()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return artists.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ArtistCell
+    
+        // Configure the cell
+        cell.updateViews(from: artists[indexPath.row])
+    
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(_ collectionView: UICollectionView, shouldHighlightItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(_ collectionView: UICollectionView, shouldShowMenuForItemAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, canPerformAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) -> Bool {
+        return false
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
+    
+    }
+    */
+    
+    func generateMockData() {
+        for index in 1...10 {
+            artists.append(Artist(name: "mi artista \(index)", genre: "mi genero \(index)"))
+        }
+    }
+
+}
